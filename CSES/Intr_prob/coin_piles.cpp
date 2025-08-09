1c16d576a76e2fbfb2b2f0bbc816c597541f6946
Uday Kumar <24b2729@iitb.ac.in>
2025-08-09 12:41:10 +0530

    some new chnages

--- CSES/Intr_prob/coin_piles.cpp
+++ CSES/Intr_prob/coin_piles.cpp
@@ -1,10 +1,11 @@
-#include <bits/stdc++.h>
+#include <iostream>
+
+#include <algorithm>
 using namespace std;
 
 int main() {
     int t;
     cin >> t;
-    
     while (t--) {
         int a, b;
         cin >> a >> b;
